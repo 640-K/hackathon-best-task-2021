eacbab1cffdd1fba90121c346e08c1acb027f872
Maksym Tkachuk <s2m4f6@gmail.com>
2021-11-27 22:26:02 +0200

    Separated main & feed. Moved Navigation. Updated Footer

--- meet-up/src/App.js
+++ meet-up/src/App.js
@@ -1,7 +1,9 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Navbar from "./Navbar"
+import Navbar from "./Navigation/Navbar"
 import Main from "./Main/Main"
+import Feed from "./Feed/Feed"
+import Footer from "./Navigation/Footer"
 import Login from "./Auth/Login/Login"
 import Register from "./Auth/Register/Register"
 import MeetCreate from './MeetCreate/MeetCreate'
@@ -11,10 +13,12 @@ const App = () => (
         <Navbar/>
         <Routes>
             <Route exact path="/" element={<Main/>}/>
+            <Route exact path="/feed" element={<Feed/>}/>
             <Route exact path="/login" element={<Login/>}/>
             <Route exact path="/register" element={<Register/>}/>
             <Route exact path="/create" element={<MeetCreate/>}/>
         </Routes>
+        <Footer/>
     </Router>
 );
 
