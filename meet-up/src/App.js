aec8106c52fabc5e2e34b6e073f385ff3cc5d879
Dionis <den70007.ua@gmail.com>
2021-11-27 16:37:21 +0200

    main files

--- meet-up/src/App.js
+++ meet-up/src/App.js
@@ -1,64 +1,18 @@
 import React from "react";
+import Main from "./Main/Main";
+import Navbar from "./Navbar";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: "rgba(226,247,255,0.66)"}}>
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+    return (
+        <Router>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Main/>}/>
+            </Routes>
+        </Router>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a className="dropdown-item" href="#">Action</a>
-                            <a className="dropdown-item" href="#">Another action</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Login</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">LogOut</a>
-                    </li>
-                </ul>
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-        </nav>
-
-        <div className="card mb-3">
-            <img className="card-img-top" src="..." alt="Card image cap"/>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.</p>
-                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-        </div>
-        <div className="card">
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-            <img className="card-img-bottom" src="..." alt="Card image cap"/>
-        </div>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ meet-up/src/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <NavLink className="navbar-brand" to="/">
+                <i className="fas fa-paw mr-1"/>
+                PetShop
+            </NavLink>
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <NavLink exact to="/" className="nav-link">Home</NavLink>
+                    </li>
+                    <li className="nav-item active">
+                        <NavLink exact to="/catalog" className="nav-link">Catalog</NavLink>
+                    </li>
+                    <li className="nav-item active">
+                        <NavLink exact to="/cart" className="nav-link">Cart
+                        </NavLink>
+                    </li>
+                </ul>
+                        <ul className="navbar-nav ml-auto">
+
+                            <li className="nav-item mr-2">
+                                <NavLink className="btn btn-outline-secondary" to="/login" type="button">Log in</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="btn btn-primary" to="/register" type="button">Register</NavLink>
+                            </li>
+                        </ul>
+
+            </div>
+
+        </nav>
+    );
+}
+export default Navbar;
