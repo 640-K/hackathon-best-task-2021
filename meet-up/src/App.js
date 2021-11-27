ca813624acad3ce29c0e2d44d568853e6dba6dcd
Wectro20 <74012654+Wectro20@users.noreply.github.com>
2021-11-27 21:52:00 +0200

    fixed nav

--- meet-up/src/App.js
+++ meet-up/src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
-import Main from "./Main/Main";
+// import Main from "./Main/Main";
 import Navbar from "./Navbar";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+// import background from "./img/background.png";
+import PrePage from "./PrePage"
+import Footer from "./Footer"
 
 function App() {
     return (
-        <Router>
-            <Navbar/>
-            <Routes>
-                <Route path="/" element={<Main/>}/>
-            </Routes>
-        </Router>
-
+        <div>
+            <Router>
+                <Navbar/>
+                {/*<Routes>*/}
+                {/*    <Route path="/" element={<Main/>}/>*/}
+                {/*</Routes>*/}
+            </Router>
+            <PrePage/>
+            <Footer/>
+        </div>
     );
 }
 
